f8ea6e160fa7e4bc6a862e22bc47a778962572a4
MarkIusBS <berkutovmark11@gmail.com>
2023-02-10 19:28:23 +0300

    Сделали новое задание

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-password=input()
-
-def ask_password(password):
-    count = 0
-    correct_password="230911"
-
-    if (password == correct_password):
-        print("доступ разрешен")
-    else:
-        print("доступ запрещен")
-
-
-print(ask_password(password))
+print("я сделал задание")

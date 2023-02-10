af84e47434c8a0b753a474cbb7dc8b7cb800527e
MarkIusBS <berkutovmark11@gmail.com>
2023-02-10 19:18:35 +0300

    Убрали три попытки ввода

--- main.py
+++ main.py
@@ -3,18 +3,11 @@ password=input()
 def ask_password(password):
     count = 0
     correct_password="230911"
-    flag = Truei
-    while (flag):
-        if (password == correct_password):
-            print("доступ разрешен")
-            flag=False
-        else:
 
-            print("Пароли не совпадают")
-            count += 1
-            password=input()
-            if count==3:
-                print("Ваши попытки кончились")
-                flag=False
+    if (password == correct_password):
+        print("доступ разрешен")
+    else:
+        print("доступ запрещен")
+
 
 print(ask_password(password))
